1877fc84f42fb5103d7af4e901e4536ddcc3c101
Michel <106674127+MichelZero@users.noreply.github.com>
2023-04-05 16:50:34 -0300

    up

--- ex04.py
+++ ex04.py
@@ -1,3 +1,9 @@
+# autores:
+# Michel
+
+# data: 05/04/2023  
+
+# 4 - dado uma tabela de dados de chuva, crie um dicionário com os dados da tabela.
 #
 # precipitação de chuva
 # jan fev mar
@@ -18,109 +24,49 @@ print("----------------")
 
 chuvas = {'jan': 40, 'fev': 30, 'mar': 20}
 
-print(chuvas)
+print(chuvas) # imprime: {'jan': 40, 'fev': 30, 'mar': 20}
 
 ###############     3     #############
 # interação
-for i in chuvas:
-  print(i)
+for i in chuvas: # percorre o dicionário 
+  print(i) # imprime as chaves do dicionário 
   
-for chave in chuvas:
-  print(chave, ' : ', end='')
-  print(chuvas[chave])
+for chave in chuvas: # percorre o dicionário
+  print(chave, ' : ', end='') # imprime as chaves do dicionário
+  print(chuvas[chave]) # imprime os valores do dicionário
 
 ###############     4     #############
 #
 #chuvas = {'jan': 40, 'fev': 30, 'mar': 20}
 #imprimir uma lista de chaves
-print(chuvas.keys()) # ['jan', 'fev', 'mar']
+print(chuvas.keys()) # imprime ['jan', 'fev', 'mar']
 
-for chave in chuvas.keys():
-  print(chuvas[chave]) # 40 30 20
+for chave in chuvas.keys(): # percorre o dicionário pegando as chaves
+  print(chuvas[chave]) # imprime os valores do dicionário
 
 ###############     5     #############
 #
 #imprimir uma lista de valores
-print(chuvas.values())
+print(chuvas.values()) # imprime [40, 30, 20]
 
-for valor in chuvas.values():
-  print(valor)
+for valor in chuvas.values(): # percorre o dicionário pegando os valores
+  print(valor) # imprime os valores do dicionário
 
 ###############     6     #############
 #
 # desempacotamento de dicionários
-print(chuvas.items())
+print(chuvas.items()) # imprime [('jan', 40), ('fev', 30), ('mar', 20)] 
 
-for chave, valor in chuvas.items():
-  print(f"chave = {chave}, tem valor = {valor}")
-
-# algumas funções 
-print(sum(chuvas.values())) # soma
-print(max(chuvas.values())) # maximo
-print(min(chuvas.values())) # minimo
-print(len(chuvas))          #tamanho
-
-print(chuvas.items())
-print(chuvas.keys())
-print(chuvas.values())
-
-
-
-####  abaixo revisão de DIC para a turma de POO em 
-
-
-#
-# precipitação de chuvas 
-# set out nov
-# 10  20  30
-#
-print("precipitação de chuvas")
-print('-'*22)
-print("set\t| out\t| nov")
-print("10\t| 20\t| 40")
-print('-'*22)
-#dicionário
-chuvas = {'set':10,'out':21,'nov':30}
-#          set      out      nov     # dict_keys(['set', 'out', 'nov'])
-print(chuvas, "-> original")
-
-''' lista = ['a','b','c']
-#         0   1   2
-#        -3  -2  -1
-
-tupla = ('a','b','c')
-#         0   1   2
-print(tupla[-1]) '''
-
-# interação
-''' for chave in chuvas: # *3
-  print(chuvas, '->',chave)  '''
-
-for chave in chuvas: 
-  print(chave, '->',chuvas[chave])
-
-print(chuvas.keys())
-print(chuvas.values())
-print(chuvas.items())
-
-for i in chuvas.keys(): # dict_keys(['set', 'out', 'nov'])
-  print(chuvas[i], ' -> ',i) 
-
-for i in chuvas.values(): # dict_values([10, 20, 30])
-  print(i, end=' ')  # 10 20 30 
-print()
-
-for i,j in chuvas.items(): # dict_items([('set', 10), ('out', 20), ('nov', 30)])
-  print(f"chave = {i}, e valor = {j}")
-  if not (j % 2 != 0):
-    print(i, "é par")
-  else:
-    print(i, "é impar")
-
-
+for chave, valor in chuvas.items(): # percorre o dicionário pegando as chaves e os valores
+  print(f"chave = {chave}, tem valor = {valor}") # imprime as chaves e os valores do dicionário
 
 # algumas funções 
-print(sum(chuvas.values())) # soma
-print(max(chuvas.values())) # maximo
-print(min(chuvas.values())) # minimo
+print(sum(chuvas.values())) # soma dos valores 
+print(max(chuvas.values())) # máximo 
+print(min(chuvas.values())) # mínimo 
 print(len(chuvas))          #tamanho
+
+print(chuvas.items()) # imprime [('jan', 40), ('fev', 30), ('mar', 20)]
+print(chuvas.keys()) # imprime ['jan', 'fev', 'mar']
+print(chuvas.values()) # imprime [40, 30, 20]
+
